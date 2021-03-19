3d189ec547f67b663e619bfcf1fa9bd1809ca627
Lee2SH000 <melisa000627@gmail.com>
2021-03-19 23:42:09 +0900

    Frame Background modify _commit 2

--- 1_create_frame.py
+++ 1_create_frame.py
@@ -8,6 +8,11 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption("My Game")
 
+
+background = pygame.image.load(
+    "C:/Users/melis/OneDrive/바탕 화면/독학실습/Py_Game/Background.png")
+
+
 running = True
 # is game running?
 
@@ -16,6 +21,9 @@ while running:
         if event.type == pygame.QUIT:  # Closing display event occur
             running = False  # Game not running
 
+    # screen.fill("#c5cae9") #setting background with color
+    screen.blit(background, (0, 0))  # setting background with png.file
 
+    pygame.display.update()  # game screen keep drawing
 # if game not running
 pygame.quit()
